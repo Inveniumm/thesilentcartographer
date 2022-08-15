307db79196c7c25403d25aa8f01ee51ceb7c0f4c
Ugur Denli <48212653+Inveniumm@users.noreply.github.com>
2022-08-15 17:01:12 +0100

    Outside View Push
    Outside View Push

--- app/src/main/java/com/invenium/thesilentcartographer/functionalities/ReplyFragment.kt
+++ app/src/main/java/com/invenium/thesilentcartographer/functionalities/ReplyFragment.kt
@@ -2,13 +2,20 @@ package com.invenium.thesilentcartographer.functionalities
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.eegeo.mapapi.EegeoApi
+import com.eegeo.mapapi.EegeoMap
 import com.eegeo.mapapi.MapView
+import com.eegeo.mapapi.services.mapscene.MapsceneRequestOptions
+import com.eegeo.mapapi.services.mapscene.MapsceneRequestResponse
+import com.eegeo.mapapi.services.mapscene.OnMapsceneRequestCompletedListener
+import com.invenium.thesilentcartographer.R
 import com.invenium.thesilentcartographer.databinding.FragmentReplyBinding
 
 
-class ReplyFragment: BaseFragment<FragmentReplyBinding>() {
+class ReplyFragment: BaseFragment<FragmentReplyBinding>(), OnMapsceneRequestCompletedListener {
     private var m_mapView: MapView? = null
+    private var m_eegeoMap: EegeoMap? = null
 
     //region BaseFragment Overrides
     override fun inflateBinding(layoutInflater: LayoutInflater): FragmentReplyBinding {
@@ -19,13 +26,28 @@ class ReplyFragment: BaseFragment<FragmentReplyBinding>() {
     }
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-
-            // You may wish to specify your API key as a string resource instead
-            context?.let { EegeoApi.init(it, "bdca9c353d2a0d4779fc7362e8ad6492") }
+            setHasOptionsMenu(true)
             m_mapView?.onCreate(savedInstanceState)
-            m_mapView?.getMapAsync { println("working") }
 
         }
+
+    override fun initViews() {
+        super.initViews()
+
+        // You may wish to specify your API key as a string resource instead
+        context?.let { EegeoApi.init(it, getString(R.string.eegeo_api_key)) }
+        m_mapView = binding.mapView
+        val listener: ReplyFragment = this
+
+        m_mapView!!.getMapAsync { map ->
+            m_eegeoMap = map
+            val mapsceneService = map.createMapsceneService()
+            mapsceneService.requestMapscene(
+                MapsceneRequestOptions("https://wrld.mp/63fcc9b")
+                    .onMapsceneRequestCompletedListener(listener)
+            )
+        }
+    }
 
     override fun onResume() {
         super.onResume()
@@ -41,4 +63,12 @@ class ReplyFragment: BaseFragment<FragmentReplyBinding>() {
         super.onDestroy()
         m_mapView?.onDestroy()
     }
+
+    override fun onMapsceneRequestCompleted(response: MapsceneRequestResponse?) {
+        if (response!!.succeeded()) {
+            val message = "Mapscene '" + response!!.mapscene.name.toString() + "' loaded"
+        }
+    }
 }
+
+
