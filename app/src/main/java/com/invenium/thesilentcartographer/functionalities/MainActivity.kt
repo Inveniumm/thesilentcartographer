e04aa9adbde32354dbf92870a244f51af1807856
Ugur Denli <ugur6@hotmail.co.uk>
2022-08-15 09:08:56 +0100

    next push
    next push

--- app/src/main/java/com/invenium/thesilentcartographer/functionalities/MainActivity.kt
+++ app/src/main/java/com/invenium/thesilentcartographer/functionalities/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.invenium.thesilentcartographer.functionalities
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.invenium.thesilentcartographer.R
+import com.invenium.thesilentcartographer.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = findNavController(R.id.navHostFragment)
+        setSupportActionBar(toolbar) //set the toolbar
+
+        navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -24,5 +36,17 @@ class MainActivity : AppCompatActivity() {
                 R.id.replyFragment
             ), drawerLayout
         )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+
+
+
 }
--- app/src/main/java/com/invenium/thesilentcartographer/functionalities/ReplyFragment.kt
+++ app/src/main/java/com/invenium/thesilentcartographer/functionalities/ReplyFragment.kt
@@ -1,7 +1,44 @@
 package com.invenium.thesilentcartographer.functionalities
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.eegeo.mapapi.EegeoApi
+import com.eegeo.mapapi.MapView
+import com.invenium.thesilentcartographer.databinding.FragmentReplyBinding
 
-class ReplyFragment: Fragment() {
 
+class ReplyFragment: BaseFragment<FragmentReplyBinding>() {
+    private var m_mapView: MapView? = null
+
+    //region BaseFragment Overrides
+    override fun inflateBinding(layoutInflater: LayoutInflater): FragmentReplyBinding {
+        return FragmentReplyBinding.inflate(layoutInflater)
+    }
+
+    override fun observeViewModel() {
+    }
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            // You may wish to specify your API key as a string resource instead
+            context?.let { EegeoApi.init(it, "bdca9c353d2a0d4779fc7362e8ad6492") }
+            m_mapView?.onCreate(savedInstanceState)
+            m_mapView?.getMapAsync { println("working") }
+
+        }
+
+    override fun onResume() {
+        super.onResume()
+        m_mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        m_mapView?.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        m_mapView?.onDestroy()
+    }
 }
